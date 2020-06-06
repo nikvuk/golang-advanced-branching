e7ce9ed554517ffd85d4abc3f1c4e996edd27ff1
Nik V <nvukovlj@gmail.com>
2020-06-06 10:51:53 +0100

    Module 1 completed

--- vehicle.go
+++ vehicle.go
@@ -46,9 +46,12 @@ const (
 	extraNegative rating = -1.2
 )
 
+var vehicleResult map[string]feedbackResult
+var inventory []vehicle
+
 func init() {
 
-	inventory := []vehicle{
+	inventory = []vehicle{
 		bike{"FTR 1200", "Indian"},
 		bike{"Iron 1200", "Harley"},
 		car{"Sonata", "Hyundai", "Sedan"},
@@ -61,7 +64,7 @@ func init() {
 		truck{"F-150", "Ford", "Truck"},
 		truck{"RAM1500", "Dodge", "Truck"}}
 
-	vehicleResult := make(map[string]feedbackResult)
+	vehicleResult = make(map[string]feedbackResult)
 
 }
 
